3aa985a28f11dc70f2e22db3c0ee8c3a880ccacc
j100cky <jaczheng@ucdavis.edu>
2025-02-04 21:40:42 -0500

    updated vegetablescreen.js

--- screens/VegetablesScreen.js
+++ screens/VegetablesScreen.js
@@ -15,9 +15,26 @@ export default function VegetablesScreen() {
     ];
   
     return (
-      <ScrollView style={styles.container}>
-        
-      </ScrollView>
+        <ScrollView style={styles.container}>
+            <Text style={styles.title}>Fresh vegetables directly from farmers land.</Text>
+            {vegetables.map((vegetable, index) => (
+                <View key={index} style={styles.itemContainer}>
+                    <Image source={{uri:vegetable.image}} style={styles.image} />
+                    <View style={styles.details}>
+                        <Text style={styles.name}>{vegetable.name}</Text>
+                        <Text style={styles.price}>{vegetable.price}</Text>
+                        <TouchableOpacity 
+                            style={styles.button} 
+                            onPress={() => alert(`${vegetable.name} added to cart`)}
+                        >
+                            <Text style={styles.buttonText}>
+                                Add to Cart
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            ))}
+        </ScrollView>
     );
   }
  
